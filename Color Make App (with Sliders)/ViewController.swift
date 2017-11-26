4b6d5b12db000db5c235aa44a3026d96a3ff8279
Oğuz Döğer <OGUZ@mb-oguz.local>
2017-11-26 14:32:29 +0300

    Initial Commit

---
+++ Color Make App (with Sliders)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Color Make App (with Sliders)
+//
+//  Created by Oğuz Döğer on 26.11.2017.
+//  Copyright © 2017 Oguz Doger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
